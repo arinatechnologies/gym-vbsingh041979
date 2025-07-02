c6fe6dae5571b9cda243e47ec9e1c77eb4c1e515
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-07-01 23:06:09 -0500

    Upload from Lambda

---
+++ js/index.js
@@ -0,0 +1,10 @@
+{
+    "heroImage": "images/index1.jpg",
+    "showcaseImage": "images/index2.jpg",
+    "icons": [
+        "images/icon-trainer.svg",
+        "images/icon-equipment.svg",
+        "images/icon-nutrition.svg",
+        "images/icon-community.svg"
+    ]
+}
