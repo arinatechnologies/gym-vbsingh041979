fdf0f02a7f089d0f285fb76b98ead12caba5c1d4
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-07-01 22:50:09 -0500

    Upload from Lambda

---
+++ js/about.js
@@ -0,0 +1,23 @@
+{
+  "hero_image": "modern gym interior with high-end equipment",
+  "timeline_images": [
+    "small gym opening day",
+    "gym expansion construction",
+    "new gym location exterior",
+    "personal trainer with client",
+    "modern gym equipment closeup"
+  ],
+  "team_images": [
+    "professional fitness trainer",
+    "nutritionist consulting client",
+    "yoga instructor in pose",
+    "group fitness instructor"
+  ],
+  "values_images": [
+    "healthy lifestyle concept",
+    "diverse group exercising together",
+    "award-winning gym facility",
+    "innovative fitness technology"
+  ],
+  "cta_image": "happy gym members working out"
+}
