ad03dcdf558b619cb4cc42eb0300d4d1f86601a1
Vikramjit Singh <vikramjitsingh@arinatechnologies.com>
2025-07-02 20:01:59 -0500

    updated

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
-
 export default {
   async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    let path = url.pathname === "/" ? "/index.html" : url.pathname;
+
     try {
-      return await getAssetFromKV({ request });
-    } catch (err) {
+      const asset = await fetch(`./${path}`, { cf: { cacheEverything: true } });
+      return asset;
+    } catch (e) {
       return new Response("Not found", { status: 404 });
     }
   },
