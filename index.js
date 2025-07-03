1ed71d83bcbd39de9be4a3f6b0db1b0c33f64b0b
Vikramjit Singh <vikramjitsingh@arinatechnologies.com>
2025-07-02 20:05:40 -0500

    static

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
+import { serveStatic } from 'wrangler/static-content';
+
 export default {
   async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-    let path = url.pathname === "/" ? "/index.html" : url.pathname;
-
-    try {
-      const asset = await fetch(`./${path}`, { cf: { cacheEverything: true } });
-      return asset;
-    } catch (e) {
-      return new Response("Not found", { status: 404 });
-    }
+    return serveStatic(request);
   },
 };
