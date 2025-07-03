e152a5855536ba7c7e0cddb917b066dd3477a765
Vikramjit Singh <vikramjitsingh@arinatechnologies.com>
2025-07-02 20:18:55 -0500

    asdf

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-import { serveStatic } from 'wrangler/static-content';
+import { getAsset } from '@cloudflare/kv-asset-handler';
+import manifest from '__STATIC_CONTENT_MANIFEST';
 
 export default {
   async fetch(request, env, ctx) {
-    return serveStatic(request);
-  },
+    try {
+      return await getAsset(request, {
+        ASSET_NAMESPACE: __STATIC_CONTENT,
+        ASSET_MANIFEST: manifest
+      });
+    } catch (err) {
+      return new Response("Not found", { status: 404 });
+    }
+  }
 };
